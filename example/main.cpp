ade1f48bec1e1081667085df4b44ff676afd3be7
CC-Dh <87935508+CC-Dh@users.noreply.github.com>
2021-12-27 01:23:22 +0530

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,10 +1,3 @@
-
-/*
-    Test Code for RP2040_irReceiver Library for receiving NEC IR Remote Codes for Raspberry Pi R2040 controller.
-    Created by CC Dharmani, on December 24, 2021.
-    Released into the public domain.
-*/
-
 #include <Arduino.h>
 #include "RP2040_irReceiver.h"
 
@@ -15,7 +8,7 @@ void setup() {
   sleep_ms(1000);
   Serial.begin(115200);
   while(!Serial);
-  Serial.println("\nIR Remote Test Code:");
+  Serial.println("\nIR Remote Test Code:\n");
   irRemote.irRxEnable(true);
 }
 
@@ -23,3 +16,15 @@ void loop() {
   // put your main code here, to run repeatedly:
   irRemote.printCode();
 }
+
+//ALternative loop() implementation:
+/*
+void loop() {
+  // put your main code here, to run repeatedly:
+  char str[50];
+  while(!irRemote.codeReceived) irRemote.receiveCode();
+  irRemote.codeReceived = 0;
+  sprintf(str,"IR Code:0x%08lx, Address:0x%02x, Command:0x%02x", irRemote.irCode, irRemote.irAdrs, irRemote.irCmd);
+  Serial.println(str);
+}
+*/
